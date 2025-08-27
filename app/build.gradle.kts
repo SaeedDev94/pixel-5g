cda8be384f62051ba34e4c1d656da133d6c8507f
SaeedDev94 <saeedp47@gmail.com>
2025-08-27 10:14:27 -0700

    v2.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "io.github.saeeddev94.pixelnr"
         minSdk = 29
         targetSdk = 36
-        versionCode = 4
-        versionName = "2.0.2"
+        versionCode = 5
+        versionName = "2.1.0"
     }
 
     compileOptions {
